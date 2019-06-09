df775a913a27911fbfca783227e06f82ade4808a
rbarcik <barcik.r@gmail.com>
2019-06-09 20:54:00 +0200

    Travel Agency Frontend

---
+++ src/main/java/com/travelagency/travelagencyfrontend/TravelAgencyFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.travelagencyfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyFrontendApplication.class, args);
+	}
+
+}
